a04de517cfb4dd73426d4759eb45afa96690e91d
Yujikawa <windbell0404@gmail.com>
2017-12-09 06:23:15 +0900

    Modify version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Numexy.Mixfile do
   def project do
     [
       app: :numexy,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "It is a library for matrix and vector calculation.",
