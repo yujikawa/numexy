ef33463aa4706461722092d699b61f5670bde064
Yujikawa <windbell0404@gmail.com>
2017-12-21 18:09:39 +0900

    Add excoveralls.

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule Numexy.Mixfile do
         licenses: ["MIT"],                       # ライセンス名、ここでは MIT にしてみました
         links: %{"GitHub" => "https://github.com/yujikawa/numexy"} # リンク集
       ],
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -27,7 +28,8 @@ defmodule Numexy.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:excoveralls, "~> 0.7", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
